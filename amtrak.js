1349c311400e260622cef73748f31ae59ddfd47b
eastsideski <eastsideski@gmail.com>
2015-05-19 22:44:59 -0400

    Fetch train data

--- amtrak.js
+++ amtrak.js
@@ -1,5 +1,38 @@
 Amtrak = (function(){
-  var amtrak = {};
+  TRAINS_URL = "https://www.googleapis.com/mapsengine/v1/tables/01382379791355219452-08584582962951999356/features?version=published&maxResults=250&key=";
 
+  function get(url, callback){
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        // Success!
+        var data = JSON.parse(this.response);
+        callback(data);
+      } else {
+       throw "HTTP Status " + this.status;
+      }
+    };
+    request.onerror = function() {
+      throw "HTTP Error";
+    };
+    request.send();
+  }
+
+  var amtrak = {
+    google_key: null,
+    getTrains: function(callback){
+      var url = TRAINS_URL + getKey();
+      get(url, callback);
+    }
+  };
+
+  var getKey = function(){
+    var key = amtrak.google_key;
+    if (!key){
+      throw "Google Key not set. Set the key with `Amtrak.google_key = KEY`";
+    }
+    return key;
+  };
   return amtrak;
 })();
